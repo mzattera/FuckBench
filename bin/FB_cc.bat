bffdf9af608cf7afd5001ddc82e0182ee2fb1f90
Massimiliano Zattera <b2fjava@gmail.com>
2020-10-11 08:36:13 +0200

    Added GCC

--- bin/FB_cc.bat
+++ bin/FB_cc.bat
@@ -2,7 +2,14 @@
 @rem This batch receives the name of the input file WITHOUT .c EXTENSION as first and unique parameter.
 
 @rem Please change the rest of the file, accordingly to your C compiler.
+
+@rem The below example is using GCC.
+
+@rem gcc -O3 6502_functional_test.c
+
 @rem The below example is using MS VS 2017 and assumes it is running in Developer Command Prompt.
 	
-cl %1.c /Fo: %1.obj /Fe: %1.exe
-@if exist %1.obj del %1.obj
+@rem cl %1.c /Fo: %1.obj /Fe: %1.exe
+@rem @if exist %1.obj del %1.obj
+
+
