096b0a27b3fbb9d24990646f10c618bc9fe3029c
Massimiliano Zattera <b2fjava@gmail.com>
2020-10-13 16:45:49 +0200

    Added batch file to run tests.

---
+++ bin/FB_run_tests.bat
@@ -0,0 +1,12 @@
+@echo off
+
+rem This runs a set of tests; in the process it rebuilds some assets too.
+rem See documentation about how to create a new release to see which parameters are available.
+
+rem Configuration
+set "FB_BIN=%~dp0%"
+if not "%FB_BIN:~-1%"=="\" set "FB_BIN=%FB_BIN%\"
+call "%FB_BIN%FB_config.bat"
+set "FB_HOME=%FB_BIN%.."
+
+java -jar "%FB_BIN%TestRunner.jar" "%FB_HOME%" %1 %2 %3 %4 %5 %6 %7 %8 %9
